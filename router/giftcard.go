d1bd75a4c995101171c991c0fd38b2518731bf4a
Hoai Ngo <hoai@autonomous.nyc>
2018-10-24 11:40:28 +0700

    update dependencies and routes

--- router/giftcard.go
+++ router/giftcard.go
@@ -9,18 +9,12 @@ import (
 type GiftCardRouter struct{}
 
 func (router GiftCardRouter) Create(routerEngine *gin.Engine) *gin.RouterGroup {
-	group := routerEngine.Group("gift-card")
-	{
-		giftCardAPI := api.GiftCardApi{}
+	group := routerEngine.Group("/gift-card")
+	giftCardAPI := api.GiftCardApi{}
 
-		orderGroup := group.Group("/order")
-		{
-			orderGroup.GET("/generate-order-id", giftCardAPI.GenerateOrderID)
-			orderGroup.POST("/create", giftCardAPI.CreateOrder)
-		}
-
-		group.POST("/check", giftCardAPI.CheckCode)
-	}
+	group.GET("/order/generate-order-id", giftCardAPI.GenerateOrderID)
+	group.POST("/order/create", giftCardAPI.CreateOrder)
+	group.POST("/check", giftCardAPI.CheckCode)
 
 	return group
 }
