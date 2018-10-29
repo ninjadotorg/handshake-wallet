c211e87ba75fe766c4dc8854bffcb1a909adeb74
Hoai Ngo <hoai@autonomous.nyc>
2018-10-29 15:34:04 +0700

    add response status for onchain api

--- api/onchain.go
+++ api/onchain.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/ninjadotorg/handshake-wallet/api_response"
 	"github.com/ninjadotorg/handshake-wallet/service"
 )
 
@@ -10,9 +11,11 @@ type OnChainApi struct{}
 // CRON JOB
 func (api OnChainApi) ScanOrders(c *gin.Context) {
 	service.OnChainServiceInst.ScanGiftCardOrders()
+	api_response.SuccessResponse(c, nil)
 }
 
 // CRON JOB
 func (api OnChainApi) ScanRedeemedCodes(c *gin.Context) {
 	service.OnChainServiceInst.ScanUsedCodes()
+	api_response.SuccessResponse(c, nil)
 }
